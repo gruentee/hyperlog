12f606b194bdb8f1920f45ac1abf3c2efaa31be3
Constantin Kraft <constantin@websolutions.koeln>
2017-11-17 17:29:26 +0100

    move hook callback methods class members

--- lib/Hook/FileHooks.php
+++ lib/Hook/FileHooks.php
@@ -8,46 +8,91 @@
 
 namespace OCA\HyperLog\Hook;
 
+use OCA\HyperLog\Service\LogService;
+use OCP\Files\Node;
+
 class FileHooks {
 
-    private $folder;
+    private $root;
+    /** @var LogService */
     private $logService;
 
-    public function __construct($folder, $logService) {
-        $this->folder = $folder;
+    public function __construct($root, $logService) {
+        $this->root = $root;
         $this->logService = $logService;
     }
 
     public function register() {
+        $reference = $this;
 
-        $callbackPostWrite = function (\OCP\Files\Node $node) {
-            $this->logService->log('Datei geschrieben', ['path' => $node->getPath()]);
+        $callbackPostWrite = function (Node $node) use ($reference) {
+            $reference->postWrite($node);
         };
-
-        $callbackPostCreate = function (\OCP\Files\Node $node) {
-            $this->logService->log('Datei erstellt', ['path' => $node->getPath()]);
+        $callbackPostCreate = function (Node $node) use ($reference) {
+            $reference->postCreate($node);
         };
-
-        $callbackPostDelete = function (\OCP\Files\Node $node) {
-            $this->logService->log('Datei gelöscht', ['path' => $node->getPath()]);
+        $callbackPostDelete = function (Node $node) use ($reference) {
+            $reference->postDelete($node);
         };
-        $callbackPostTouch = function (\OCP\Files\Node $node) {
-
+        $callbackPostTouch = function (Node $node) use ($reference) {
+            $reference->postTouch($node);
         };
-        $callbackPostCopy = function (\OCP\Files\Node $node) {
-
+        $callbackPostCopy = function (Node $node) use ($reference) {
+            $reference->postCopy($node);
         };
-        $callbackPostRename = function (\OCP\Files\Node $node) {
-
+        $callbackPostRename = function (Node $node) use ($reference) {
+            $reference->postDelete($node);
         };
-
 
         // register listeners for file system events
-        $this->folder->listen('\OCP\Files\Node', 'postWrite', $callbackPostWrite);
-        $this->folder->listen('\OCP\Files\Node', 'postCreate', $callbackPostCreate);
-        $this->folder->listen('\OCP\Files\Node', 'postDelete', $callbackPostDelete);
-        $this->folder->listen('\OCP\Files\Node', 'postTouch', $callbackPostTouch);
-        $this->folder->listen('\OCP\Files\Node', 'postCopy', $callbackPostCopy);
-        $this->folder->listen('\OCP\Files\Node', 'postRename', $callbackPostRename);
+        $this->root->listen('\OC\Files', 'postWrite', $callbackPostWrite);
+        $this->root->listen('\OC\Files', 'postCreate', $callbackPostCreate);
+        $this->root->listen('\OC\Files', 'postDelete', $callbackPostDelete);
+        $this->root->listen('\OC\Files', 'postTouch', $callbackPostTouch);
+        $this->root->listen('\OC\Files', 'postCopy', $callbackPostCopy);
+        $this->root->listen('\OC\Files', 'postRename', $callbackPostRename);
     }
+
+    /**
+     * @param Node $node
+     */
+    public function postWrite(Node $node) {
+        $this->logService->log('Datei geschrieben', ['path' => $node->getPath()]);
+    }
+
+    /**
+     * @param Node $node
+     */
+    public function postCreate(Node $node) {
+        $this->logService->log('Datei erstellt', ['path' => $node->getPath()]);
+    }
+
+    /**
+     * @param Node $node
+     */
+    public function postDelete(Node $node) {
+        $this->logService->log('Datei gelöscht', ['path' => $node->getPath()]);
+    }
+
+    /**
+     * @param Node $node
+     */
+    public function postTouch(Node $node) {
+        $this->logService->log('Datei angesehen', ['path' => $node->getPath()]);
+    }
+
+    /**
+     * @param Node $node
+     */
+    public function postCopy(Node $node) {
+        $this->logService->log('Datei kopiert', ['path' => $node->getPath()]);
+    }
+
+    /**
+     * @param Node $node
+     */
+    public function postRename(Node $node) {
+        $this->logService->log('Datei umbenannt', ['path' => $node->getPath()]);
+    }
+
 }
